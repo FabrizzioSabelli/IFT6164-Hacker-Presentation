Trying to find mistake in jacobian augmentation or FSGM

src/attacks.py
@@ -8,14 +8,16 @@ from tqdm import tqdm
 from torch.nn.functional import cross_entropy
 import os
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import sklearn.metrics as metrics
 
 
 class jointDataset(Dataset):
 
-    def __init__(self, testset_dir, adversial_dir):
+    def __init__(self, testset, adversialset):
 
-        self.testset_dir = testset_dir
-        self.adversial_dir = adversial_dir
+        self.testset = testset
+        self.adversialset = adversialset
 
     def __len__(self):
         return len(
@@ -23,8 +25,8 @@ class jointDataset(Dataset):
         )  # one or the other should be fine as they both have same length
 
     def __getitem__(self, idx):
-        test_image = torch.load(self.testset_dir + f"/image_{idx}.pt")
-        adversial_image = torch.load(self.adversial_dir + f"/image_{idx}.pt")
+        test_image = torch.load(self.testset.__getitem__(idx))
+        adversial_image = torch.load(self.adversialset.__getitem__(idx))
         return test_image, adversial_image
 
 
@@ -63,6 +65,12 @@ class AdversarialDataset(Dataset):
         image = torch.load(img_path)
 
         return image
+
+    def get_annotations(self, epsilon, attack_type):
+        annotations_path = (
+            self.image_dir + f"/{attack_type}/" + f"/{epsilon}/" + "annotations.csv"
+        )
+        return pd.read_csv(annotations_path)
 
     # Fast-Gradient Sign Method for computing adversial attacks
     def attack_FGSM(self, substitute, oracle, epsilon, batch_size=32):
@@ -136,26 +144,25 @@ class AdversarialDataset(Dataset):
                     new_image_path,
                 )
 
-        self.annotations_df = pd.DataFrame(rows)
-        self.annotations_df.to_csv(image_path + "/annotations.csv", index=False)
+        annotations_df = pd.DataFrame(rows)
+        annotations_df.to_csv(image_path + "/annotations.csv", index=False)
 
         # return transferability and attack success rate on oracle and substitute
         substitute_success_frac = (
-            self.annotations_df["sub_label"] != self.annotations_df["adv_sub_label"]
+            annotations_df["sub_label"] != annotations_df["adv_sub_label"]
         ).sum()
 
         oracle_success_frac = (
-            self.annotations_df["oracle_label"]
-            != self.annotations_df["adv_oracle_label"]
+            annotations_df["oracle_label"] != annotations_df["adv_oracle_label"]
         ).sum()
-        substitute_success = substitute_success_frac / len(self.annotations_df.index)
-        oracle_success_succss = oracle_success_frac / len(self.annotations_df.index)
+        substitute_success = substitute_success_frac / len(annotations_df.index)
+        oracle_success_succss = oracle_success_frac / len(annotations_df.index)
 
         print(
-            f"Substitute attack success rate: {substitute_success}, fraction: {substitute_success_frac}/{len(self.annotations_df.index)}"
+            f"Substitute attack success rate: {substitute_success}, fraction: {substitute_success_frac}/{len(annotations_df.index)}"
         )
         print(
-            f"Oracle attack success rate: {oracle_success_succss}, fraction: {oracle_success_frac}/{len(self.annotations_df.index)}"
+            f"Oracle attack success rate: {oracle_success_succss}, fraction: {oracle_success_frac}/{len(annotations_df.index)}"
         )
 
         # attack finished
@@ -172,10 +179,14 @@ class AdversarialDataset(Dataset):
         oracle_true = []
         oracle_adversial = []
 
+        self.annotations_df = self.get_annotations(
+            epsilon, attack_type
+        )  # so we know the length of the adversial dataset
+
         # update epsilon so goes into correct folder
         self.epsilon = epsilon
         self.attack_type = attack_type
-        jointD = jointDataset(self.test_set, self.image_dir)
+        jointD = jointDataset(self.test_set, self)
         jointLoader = DataLoader(jointD, batch_size=batch_size, shuffle=False)
         # create the adversial images
         for idx, (x_test), (x_adv) in tqdm(
@@ -192,6 +203,8 @@ class AdversarialDataset(Dataset):
 
         # Find the indices where the arrays differ
         num_same_positions = np.sum(oracle_true_np != oracle_adversial_np)
+
+        # get annotations
 
         # Count the number of different positions
         attack_success_frac = len(num_same_positions)
@@ -217,7 +230,7 @@ class AdversarialDataset(Dataset):
             param.requires_grad = True
 
         # create adversial examples
-        x_adv = x + epsilon * x.grad.sign()
+        x_adv = x + epsilon * torch.sign(x.grad)
         return x_adv
 
     # Saliency attack from Papernot et al.
@@ -225,7 +238,7 @@ class AdversarialDataset(Dataset):
         # need to remember to put attack type and the epislon thing as in FGSM
         pass
 
-    def show_adversial_progress(self, model, image_id, attack_type):
+    def show_adversial_progress(self, model, attack_type, num_output):
         # function used to show the adversial image created
         # image id goes from 0 to 9849
         fig, ax = plt.subplots(figsize=(15, 15), nrows=1, ncols=5)
@@ -244,12 +257,49 @@ class AdversarialDataset(Dataset):
 
             # set epsilon value to go into correct folder
             self.epsilon = epsilon
+            annotations_df = self.get_annotations(epsilon, attack_type)
+            cm = self.get_confusion_matrices(
+                y_pred=annotations_df["sub_label"],
+                y_adv=annotations_df["adv_sub_label"],
+            )
+
+            cm_display = metrics.ConfusionMatrixDisplay(
+                confusion_matrix=confusion_matrix,
+                display_labels=list(range(num_output)),
+            )
+
+            # TODO make this work for CIFAR-10 since we remove the first channel to have 28x28
+            # ax[i].imshow(image.squeeze(), cmap="gray")
+            # ax[i].set_title(f"epsilon = {epsilon} confusion matrix")
+
+        plt.show()
+
+    def show_cm_progress(self, model, image_id, attack_type):
+        # function used to show the model accuracy given epsilon values for a agiven attack type
+        # image id goes from 0 to 9849
+        fig, ax = plt.subplots(figsize=(15, 15), nrows=1, ncols=5)
+
+        #
+        if attack_type == "fgsm":
+            epsilons = self.epsilons_fgsm
+        else:
+            epsilons = self.epsilons_saliency
+
+        # set attack type to get correct folder
+        self.attack_type = attack_type
+        # this should sorted in increasing order
+        for i, epsilon in enumerate(epsilons):
+            # set epsilon value to go into correct folder
+            self.epsilon = epsilon
             image = self.__getitem__(image_id)
             pred = model.predict(image).item()
 
-            # TODO make this work for CIFAR-10
-            ax[i].imshow(image.cpu().squeeze(), cmap="gray")
-            ax[i].set_title(f"ε = {epsilon} pred={pred}")
+            # TODO make this work for CIFAR-10 since we remove the first channel to have 28x28
+            ax[i].imshow(image.squeeze(), cmap="gray")
+            ax[i].set_title(f"epsilon = {epsilon} pred={pred}")
             ax[i].axis("off")
 
-        plt.show()
+    def get_confusion_matrices(self, y_pred, y_adv):
+        # get the confusion matrices for the oracle and the substitute after adverisal attacks
+
+        return confusion_matrix(np.array(y_pred), np.array(y_adv))